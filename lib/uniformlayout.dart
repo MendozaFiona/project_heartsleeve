df8fc344ab0996dae551b819bbc6363e752b42fa
unknown <lenalee.mendoza@gmail.com>
2020-12-14 07:30:19 +0800

    added and edited bottom navigation bar

--- lib/uniformlayout.dart
+++ lib/uniformlayout.dart
@@ -15,13 +15,13 @@ class UniformLayout extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-              colorFilter: ColorFilter.mode(Color.fromRGBO(229,229,229,1).withOpacity(0.2), BlendMode.dstATop),
+              colorFilter: ColorFilter.mode(Color.fromRGBO(254,250,250,1).withOpacity(0.2), BlendMode.dstATop),
               image: AssetImage("assets/images/heartsleeve_icon.png"),
               fit: BoxFit.scaleDown,
               //scale: 0.8,
           ),
 
-          color: Color.fromRGBO(229,229,229,1),      
+          color: Color.fromRGBO(254,250,250,1),      
 
         ),
         child: HomePage(), //change to be dynamic
@@ -34,14 +34,50 @@ class UniformLayout extends StatelessWidget {
         BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.business)/*Image.asset("assets/images/write.png")*/,
-              label: 'home',
+              icon: ImageIcon(
+                AssetImage("assets/images/posts.png"),
+              ),
+              label: 'dashboard',
             ),
+
             BottomNavigationBarItem(
-              icon: Icon(Icons.home)/*Image.asset("assets/images/write.png")*/,
-              label: 'test',
-            )
+              icon: ImageIcon(
+                AssetImage("assets/images/write.png")
+              ),
+              label: 'write',
+            ),
+
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/images/discover.png")
+              ),
+              label: 'discover',
+            ),
+
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/images/bookmarked.png")
+              ),
+              label: 'bookmarks',
+            ),
+
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/images/profile.png")
+              ),
+              label: 'account',
+            ),
+
           ],
+
+          backgroundColor: Color.fromRGBO(106,65,98,1),
+          selectedItemColor: Color.fromRGBO(243,157,182,1),
+          unselectedItemColor: Color.fromRGBO(254,250,250,1),
+
+          selectedFontSize: 11.0,
+          unselectedFontSize: 11.0,
+
+          type : BottomNavigationBarType.fixed
         
         )
       
